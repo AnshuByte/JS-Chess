037cb911f836bc657e4ec44cb523e3206fefd655
anshurauniyar17 <anshurauniyar2000@gmail.com>
2024-01-23 23:43:41 +0530

    color change

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function createBoard(){
         square.firstChild && square.firstChild?.setAttribute ('draggable',true),
         square.setAttribute('square-id',i)
         // square.classList.add("beige")
-        const row = Math.floor(63- i/8) + 1
+        const row = Math.floor((63- i)/8) + 1
         if (row % 2 === 0){
             square.classList.add(i% 2===0 ? "beige" : "brown")
         }else{
