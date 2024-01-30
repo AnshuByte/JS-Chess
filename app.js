9bf2f37557a70271bc88fef994627ea059c95474
anshubyte <anshurauniyar2000@gmail.com>
2024-01-31 00:08:12 +0530

    update app.js

--- app.js
+++ app.js
@@ -64,11 +64,66 @@ function dragOver(e){
 function dragDrop(e){
     e.stopPropagation()
     console.log(e.target);
+    const correctGo = draggedElement.firstChild.classList.contains(playerGo)
     const taken = e.target.classList.contains('piece')
+    const valid = checkIfValid(e.target)
+    const opponentGo = playerGo ==='white'? 'black' : 'white'
+    const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo) 
+
+    if (correctGo){
+        //must check first
+        if(takenByOpponent && valid)
+        {
+            e.target.parentNode.append(draggedElement)
+            e.target.remove()
+            changePlayer()
+            return
+        }
+        //2nd check
+    if(taken && !takenByOpponent){
+        infoDisplay.textContent = "you cannot go here!"
+        setTimeout(() => infoDisplay.textContent="",2000)
+        return
+    }
+    if (valid){
+        e.target.append(draggedElement)
+        changePlayer()
+        }
+    } 
+} 
+    
     // e.target.parentNode.append(draggedElement)
     // e.target.remove()
     // e.target.append(draggedElement)
-    changePlayer()
+    // changePlayer()
+
+
+
+
+
+
+
+function checkIfValid(target){
+     const targetId = Number(target.getAttribute('square-id')) || Number(target.parentNode.getAttribute('square-id'))
+     const startId = Number(startPositionId)  
+     const piece = draggedElement.id
+     //console.log(targetId)
+     switch(piece)
+    {
+    case 'pawn':
+        const starterRow = [8,9,10,11,12,13,14,15]
+        if 
+        (
+        starterRow.includes(startId) && startId + width *2 === targetId
+        || startId + width *2=== targetId||
+        startId + width - 1 === targetId && document.querySelector(`[square-id="${startId + width - 1}]`).firstChild ||
+        startId + width + 1 === targetId && document.querySelector(`[square-id="${startId + width + 1}]`).firstChild
+
+        ){
+          return true
+        }
+
+    }
 }
 
 function changePlayer(){
